b2420501713986ef7accf08f4a0e29df8d1fa342
sturley <sturley@DESKTOP-STEVEN>
2022-10-31 13:42:56 -0400

    Add project files.

---
+++ Adventures/Adventure.cs
@@ -0,0 +1,26 @@
+﻿namespace ConsoleTestRpg.Adventures
+{
+    public class Adventure
+    {
+        public string? GUID { get; set; }
+        public string? Title { get; set; }
+        public string? Description { get; set; }
+        public int ExpReward { get; set; }
+        public int GoldReward { get; set; }
+        public int MaxLevel { get; set; }
+        public int MinimumLevel { get; set; }
+        public Adventure()
+        {
+
+        }
+
+
+        /// <summary>
+        /// this class dose this and that  and don't do this.
+        /// </summary>
+        public void myMethod()
+        {
+
+        }
+    }
+}
---
+++ Game/GameService.cs
@@ -0,0 +1,33 @@
+﻿using ConsoleTestRpg.Adventures;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleTestRpg.Gamw
+{
+    public class GameService
+    {
+        public void StartGame()
+        {
+            var basePath = $"{AppDomain.CurrentDomain.BaseDirectory}Adventures";
+            var initialAdventure = new Adventure();
+
+            if (File.Exists($"{basePath}\\initial.json"))
+            {
+                var directory = new DirectoryInfo(basePath);
+                var initialJsonFile = directory.GetFiles("initial.json");
+
+                using (StreamReader fi = File.OpenText(initialJsonFile[0].FullName))
+                {
+                    initialAdventure = JsonConvert.DeserializeObject<Adventure>(fi.ReadToEnd());
+                }
+
+                Console.WriteLine($"Adventure : {initialAdventure.Title}");
+                Console.WriteLine($"Description : {initialAdventure.Description}");
+            }
+        }
+    }
+}
